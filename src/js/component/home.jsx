4d18ff7ad8457a9d8d549fb74f78e8dee8afaf25
KGTH <kgthcruz94@gmail.com>
2022-11-10 13:57:53 +0000

    se genera semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+
+
+	const [ semaforoColor , setsemaforoColor ] = useState("semaforoRed");
+	
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		
+		<div className=" container ">
+			<div className="semaforo ">
+				<div className="semaforoRed "  onClick={() => setsemaforoColor()} ></div>
+			<div className="semaforoYellow "  onClick={() => setsemaforoColor()}></div>
+			<div className="semaforoGreen " onClick={() => setsemaforoColor()}></div>
+			</div>
+			</div>
+
+		
 	);
 };
 
